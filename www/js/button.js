0ebe65a84f25e3a70a4063c05320471ae9e94fce
Linette V Maliakal <lmaliakal21@gmail.com>
2019-04-09 13:18:41 -0500

    adding field for event title, location, address, time/date
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- www/js/button.js
+++ www/js/button.js
@@ -1,11 +1,3 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  if (page.id === 'Tab2') {
     page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('event_instance.html', {data: {title: 'Events Page'}});
+      document.querySelector('#tab2.html').pushPage('event_instance.html', {data: {title: 'Events Page'}});
     };
-  } else if (page.id === 'Event_Instance') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
-});
