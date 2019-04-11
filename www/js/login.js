3533da9e691a5fec8d10f4238fc88c9590bba6df
Maya <mgocalkappos@luc.edu>
2019-04-11 16:04:20 -0500

    removed unnecessary file

--- www/js/login.js
+++
@@ -1,7 +0,0 @@
-//const db = firebase.database();
-var login = function() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-
-  console.log('login button clicked');
-}
