b50f3d0a455e20c7a2d20349622ca13c22e9d0f9
macabrera9 <mcabrera2@luc.edu>
2019-04-16 23:07:18 -0500

    added saved event page

--- www/js/pushnext.js
+++ www/js/pushnext.js
@@ -5,6 +5,11 @@ document.addEventListener('init', function(event) {
     page.querySelector('#push-button').onclick = function() {
       document.querySelector('#myNav').pushPage('event_instance.html', {data: {title: 'Event Form'}});
     };
+  }
+  if (page.id === 'Event_Instance') {
+    page.querySelector('#push-button').onclick = function() {
+      document.querySelector('#myNav').pushPage('tab2CLONE.html', {data: {title: 'Tab 2 Clone'}});
+    };
   } else if (page.id === 'Event_Instance') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
