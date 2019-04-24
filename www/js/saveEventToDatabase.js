62cf6848e53f92b71879616ced17dcacf4d9d448
Michael Cabrera <mikey@Michaels-MacBook-Pro.local>
2019-04-24 10:59:31 -0500

    added save event js

--- www/js/saveEventToDatabase.js
+++ www/js/saveEventToDatabase.js
@@ -7,14 +7,14 @@ function writeNewEvent(uid, username, title, location, date) {
       location: location,
       date: date
     };
-  
+
     // Get a key for a new Event.
     var newEventKey = firebase.database().ref().child('events').push().key;
-  
+
     // Write the new post's data simultaneously in the posts list and the user's post list.
     var updates = {};
     updates['/events/' + newEventKey] = eventData;
-    updates['/user-events/' + uid + '/' + newPostKey] = eventData;
+    updates['/user-events/' + uid + '/' + newEventKey] = eventData;
   
     return firebase.database().ref().update(updates);
 }
