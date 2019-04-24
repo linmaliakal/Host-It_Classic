a9e738e9822785d07042dfacd15f9ee03a831c62
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-04-24 09:25:42 -0500

    added ability to store events in database

---
+++ www/js/saveEventToDatabase.js
@@ -0,0 +1,20 @@
+function writeNewEvent(uid, username, title, location, date) {
+    // An event entry.
+    var eventData = {
+      uid: uid,
+      owner: username,
+      title: title,
+      location: location,
+      date: date
+    };
+  
+    // Get a key for a new Event.
+    var newEventKey = firebase.database().ref().child('events').push().key;
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/events/' + newEventKey] = eventData;
+    updates['/user-events/' + uid + '/' + newPostKey] = eventData;
+  
+    return firebase.database().ref().update(updates);
+}
