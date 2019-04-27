b4838fb7344bc0c1cae59ef8bdce07251c94b630
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-04-26 21:28:12 -0500

    fixed card title info

--- www/js/newevent.js
+++ www/js/newevent.js
@@ -1,14 +1,17 @@
 function newEvent(eventData) {
   var newCard = document.createElement("ons-card");
-  console.log(eventData);
-  var eventTitle = "eventData.title";
+  var eventTitle = eventData.title;
   var textNode = document.createTextNode(eventTitle);
   newCard.appendChild(textNode);
+  newCard.appendChild(document.createTextNode(eventData.date));
   newCard.setAttribute("id", "eventData.uid");
   var navigate = document.getElementById("myNav");
   newCard.onclick = function(eventTitle){
-    navigate.pushPage('refresher.html', { animation : 'slide' });
+    navigate.pushPage('refresher.html', { 
+      data : {
+        title: 'example'
+      },
+      animation : 'slide' });
   };
   document.getElementById("eventsList").appendChild(newCard);
-  alert("Event card titled " + eventTitle + " added to Events Page!");
 }
