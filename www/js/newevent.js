6867cfabe53cb4cbd3dcc06f33a1b05daf7f67df
ANTHONY-XPS13\Anthony <alifonti@luc.edu>
2019-04-26 23:09:24 -0500

    contextual info now shows in refresher page

--- www/js/newevent.js
+++ www/js/newevent.js
@@ -1,19 +1,27 @@
 function newEvent(eventData) {
   var newCard = document.createElement("ons-card");
+  //things
   var eventTitle = eventData.title;
+  var eventLocation = eventData.location;
+  var eventDate = eventData.date;
+  var eventOwner = eventData.owner;
+
   var textNode = document.createTextNode(eventTitle);
   newCard.appendChild(textNode);
   newCard.appendChild(document.createTextNode(eventData.date));
   newCard.setAttribute("id", "eventData.uid");
   var navigate = document.getElementById("myNav");
   newCard.onclick = function(eventData){
-    navigate.pushPage('refresher.html', { 
-      data : {
-        title: eventData.title,
-        location: eventData.location,
-        owner: eventData.owner,
+    var options = {
+      data: {
+        title: eventTitle,
+        location: eventLocation,
+        date: eventDate,
+        owner: eventOwner
       },
-      animation : 'slide' });
-  };
+      animation: 'slide'
+      };
+    navigate.pushPage('refresher.html', options);
+  }
   document.getElementById("eventsList").appendChild(newCard);
 }
